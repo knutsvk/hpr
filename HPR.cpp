29b88ac97533173ff9c54ca587298353056a793d
knutsvk <ksk38@cam.ac.uk>
2016-08-05 14:50:41 +0200

    went back to 1e-6 tol

--- HPR.cpp
+++ HPR.cpp
@@ -1141,7 +1141,7 @@ double HPR_Fluid::getTimeStep( const double c_CFL )
 void HPR_Fluid::integrateODE( double dt )
 {
     int cell; 
-    double tol = 1.0e-16;
+    double tol = 1.0e-6;
     int nSteps = 0;
 
 #pragma omp parallel for private( cell ) reduction( +:nSteps )
