59d382fd20e9fb716d48016f2916e1b4ca91a057
knutsvk <ksk38@cam.ac.uk>
2016-08-05 12:32:45 +0200

    done with stokes first man

--- MaxError.py
+++ MaxError.py
@@ -2,10 +2,10 @@ import numpy as np
 from scipy import special
 from numpy import linalg as la
 
-for i in [2, 4, 8, 16]:
+for i in [1, 2, 4, 8]:
     r = np.genfromtxt('./StokesFirstProblem_1DX_Nx' + str(10 * i) + '_Ny' +
             str(i) + '_100.out', delimiter='\t', dtype=float, names=True)
-    eps = abs(r['v'] / (0.1 * special.erf(5.0 * r['x'])) - 1.0)
+    eps = abs(r['v'] - (0.1 * special.erf(5.0 * r['x'])) )
     m1 = eps[eps.argmax()]
     m2 = np.mean(eps)
     m3 = la.norm(eps)
